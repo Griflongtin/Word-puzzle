ddc94f9356b1abe59a0f7688853773e15ef5508f
Griffin P Longtin and Michael Pinaud <griffin@>
2017-10-24 13:35:18 -0700

    add loop function in jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(function() {
   $("form").submit(function(event){
-    var vowels = ["a", "e", "i", "o", "u"];
     event.preventDefault();
-      var userInput = $("input#user-sentence").val();
-
-      for ( var number = 0; number <= userInput.length; number += 1) {
-        vowels.forEach(function(vowel) {
-           userInput = userInput.replace(vowel, "-");
-        });
-      }
-      $("form").hide();
-      $("#output").append("<p>" + userInput + "</p>");
+    var vowels = ["a", "e", "i", "o", "u"];
+    var letters = $("input#user-sentence").val().split("");
+    for (var iletter = 0; iletter < letters.length; iletter++) {
+      for (var ivowel = 0; ivowel < vowels.length; ivowel++) {
+        if (letters[iletter] === vowels[ivowel]){
+        letters[iletter] = "-";
+        };
+      };
+    };
+    $("#output").append(letters.join(""));
   });
 });
