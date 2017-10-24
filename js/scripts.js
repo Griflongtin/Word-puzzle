b6e7d75e0f13a3443d4939d19c3c559313eb59fc
Griffin P Longtin and Michael Pinaud <michael@>
2017-10-24 11:20:46 -0700

    this used regular expressions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(function() {
-  $("form-group").submit(function(event){
+  $("form").submit(function(event){
+    var vowels = ["a", "e", "i", "o", "u"];
     event.preventDefault();
-      var userInput = [];
-      var 
+      var userInput = $("input#user-sentence").val();
 
+      for ( var number = 0; number <= userInput.length; number += 1) {
+        vowels.forEach(function(vowel) {
+           userInput = userInput.replace(vowel, "-");
+        });
+      }
+      $("form").hide();
+      $("#output").append("<p>" + userInput + "</p>");
   });
 });
